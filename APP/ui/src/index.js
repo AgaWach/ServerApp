28aa0404c3863419add3153a97171a096260252c
Agnieszka Wach <wach.agnieszka@gmail.com>
2019-09-22 21:33:50 +0200

    Dropdown Menu

--- APP/ui/src/index.js
+++ APP/ui/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { MuiThemeProvider, createMuiTheme} from '@material-ui/core';
-import { blueGrey } from '@material-ui/core/colors';
+import { indigo } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
   palette: {
-    primary: blueGrey,
-    secondary: {
-      main: '#37474f',
-    }
+    primary: indigo,
   }
 })
 
